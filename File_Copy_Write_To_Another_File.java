a2557644733f0410c28ff959dd9f42feb0dabd06
Nithish M <89405692+nithishgwd@users.noreply.github.com>
2022-10-22 18:17:06 +0530

    Update File_Copy_Write_To_Another_File.java

--- File_Copy_Write_To_Another_File.java
+++ File_Copy_Write_To_Another_File.java
@@ -25,28 +25,34 @@ public class File_Copy_Write_To_Another_File{
                 System.out.println("File exist");
             }
             
-            //FileWriter f1w = new FileWriter("read.txt");
-            //f1w.write("welcome to java program");
-            //f1w.close();
-            
-            String s = "";
+            String s = "",c="";
             LocalTime i;
-            int j;
+            int j,h;
             FileReader f1r = new FileReader("Input.txt");
             while((j=f1r.read())!=-1) {
                 s += (char)j;
             }
-            System.out.println(s);
+            
+            f1r.close();
+            System.out.println("\n-> Data Copied From Input File\n\n"+ s);
             FileWriter fw = new FileWriter("Output.txt",true);
-            fw.write("\n\n");
+            fw.write("\n");
             
             i=java.time.LocalTime.now();
             
             fw.write(i.toString());
             fw.write("\n");
             fw.write(s);
-            f1r.close();
             fw.close();
+            
+            FileReader ro = new FileReader("Output.txt");
+            while((h=ro.read())!=-1) {
+                c += (char)h;
+            }
+            System.out.println("\n->Data present in Output File\n\n");
+            System.out.println(c);
+            ro.close();
+            
         }
         catch(IOException exception){
             System.out.println(" Unfortunately an error has been occurred. ");  
